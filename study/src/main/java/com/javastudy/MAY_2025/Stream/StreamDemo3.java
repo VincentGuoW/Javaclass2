915643459de9f8d8fb1b8e6f27910fc75cc617af
VincentGuoW <sdevincentg@gmail.com>
2025-05-21 23:12:43 -0400

    functional interface has only one abstract method need to rewrite

---
+++ study/src/main/java/com/javastudy/MAY_2025/Stream/StreamDemo3.java
@@ -0,0 +1,26 @@
+package com.javastudy.MAY_2025.Stream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.function.Predicate;
+
+public class StreamDemo3 {
+    public static void main(String[] args) {
+        //Functional interface has one and only abstract method
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aa");
+        list.add("eb");
+        list.add("rd");
+        list.add("ae");
+        list.add("fq");
+
+        list.stream().filter(new Predicate<String>() {
+
+            @Override
+            public boolean test(String t) {
+                return t.startsWith("a");
+            }
+            
+        }).forEach(s->System.out.println(s));;
+    }
+}
