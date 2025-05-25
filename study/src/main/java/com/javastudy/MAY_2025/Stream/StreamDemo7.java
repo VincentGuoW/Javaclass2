01dd5c96aa753adb9ee93a5593bc187b5077963f
VincentGuoW <sdevincentg@gmail.com>
2025-05-25 14:59:37 -0400

    test

--- study/src/main/java/com/javastudy/MAY_2025/Stream/StreamDemo7.java
+++ study/src/main/java/com/javastudy/MAY_2025/Stream/StreamDemo7.java
@@ -1,6 +1,8 @@
 package com.javastudy.MAY_2025.Stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.IntFunction;
 
 public class StreamDemo7 {
     public static void main(String[] args) {
@@ -25,5 +27,28 @@ public class StreamDemo7 {
         long listLong = list.stream().count();
         System.out.println(listLong);
 
-    }
-}
+        System.out.println("--------------------------------------");
+        Object[] listArr = list.stream().toArray();
+        System.out.println(Arrays.toString(listArr));
+
+        System.out.println("--------------------------------------");
+        String result2 = Arrays.toString(list.stream().toArray(new IntFunction<String[]>() {
+
+            @Override
+            public String[] apply(int value) {
+               return new String[value];  
+            }
+            
+        }));
+
+        System.out.println(result2);
+
+        System.out.println("--------------------------------------");
+        String result3 = Arrays.
+            toString(list.stream().
+                toArray(value -> new String[value] )
+            );
+
+        System.out.println(result3);
+
+}}
