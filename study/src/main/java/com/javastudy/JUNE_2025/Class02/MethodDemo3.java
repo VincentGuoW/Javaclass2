82c0970d36fe9c2410b14141c71e2f5c2cb5f1b4
VincentGuoW <sdevincentg@gmail.com>
2025-06-12 14:42:48 -0400

    test

--- study/src/main/java/com/javastudy/JUNE_2025/Class02/MethodDemo3.java
+++ study/src/main/java/com/javastudy/JUNE_2025/Class02/MethodDemo3.java
@@ -1,4 +1,4 @@
-package com.javastudy.June_2025;
+package com.javastudy.June_2025.Class02;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- study/src/main/java/com/javastudy/JUNE_2025/Class02/StringOperation.java
+++ study/src/main/java/com/javastudy/JUNE_2025/Class02/StringOperation.java
@@ -1,4 +1,4 @@
-package com.javastudy.June_2025;
+package com.javastudy.June_2025.Class02;
 
 public class StringOperation {
     public static  boolean test(String s ){
