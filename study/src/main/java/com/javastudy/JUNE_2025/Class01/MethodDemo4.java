9f76aa71e27cef7f2878132a52ad4188978a2e20
VincentGuoW <sdevincentg@gmail.com>
2025-06-11 23:10:42 -0400

    this can't be in static method

--- study/src/main/java/com/javastudy/JUNE_2025/Class01/MethodDemo4.java
+++ study/src/main/java/com/javastudy/JUNE_2025/Class01/MethodDemo4.java
@@ -4,22 +4,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class MethodDemo4 {
-    public void main(String[] args) {
-        new MethodDemo4.run();
+    public static void main(String[] args) {
+        new MethodDemo4().run();
 
     }
 
-    public static boolean test4(String s) {
-        return s.startsWith("T") && s.length() == 3;
-    }
+    
 
-    public class run {
+    public void run (){
         ArrayList<String> nameList5 = new ArrayList<>();
         Collections.addAll(nameList5, "Sam", "Tom", "Henry", "Tommy", "Toby");
 
         nameList5.stream()
                  .filter(this::test4) 
                  .forEach(System.out::println);
+    }
+
+    public  boolean test4(String s) {
+        return s.startsWith("T") && s.length() == 3;
     }
 
 }
