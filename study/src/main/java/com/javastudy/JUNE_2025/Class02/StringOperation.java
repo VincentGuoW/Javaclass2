912897aa19ff8fcdc86c491fdb270f33efc09086
VincentGuoW <sdevincentg@gmail.com>
2025-06-17 09:45:48 -0400

    test

--- study/src/main/java/com/javastudy/JUNE_2025/Class02/StringOperation.java
+++ study/src/main/java/com/javastudy/JUNE_2025/Class02/StringOperation.java
@@ -2,6 +2,6 @@ package com.javastudy.June_2025.Class02;
 
 public class StringOperation {
     public static  boolean test(String s ){
-        return s.startsWith("T") && s.length()==3;
+        return s.startsWith("T") && s.length()==3;//test
     }
 }
