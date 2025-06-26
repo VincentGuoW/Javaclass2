2abf149e0022cea9684b00a9db9f998c889b443d
VincentGuoW <sdevincentg@gmail.com>
2025-06-26 15:32:26 -0400

    exception

---
+++ study/src/main/java/com/javastudy/JUNE_2025/Class05/ErrorAndException1.java
@@ -0,0 +1,11 @@
+package com.javastudy.JUNE_2025.Class05;
+
+import com.javastudy.JUNE_2025.Class04.Student;
+
+public class ErrorAndException1 {
+     public static void main(String[] args) {
+        Student[]  arr = new Student[3];
+        String name = arr[0].getName();
+        sout
+     }
+}
