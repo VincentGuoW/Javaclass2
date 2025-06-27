0775a785e44180c52b58c065b9978c88a735cca2
VincentGuoW <sdevincentg@gmail.com>
2025-06-27 07:46:57 -0400

    error test

--- study/src/main/java/com/javastudy/JUNE_2025/Class05/ErrorAndException2.java
+++ study/src/main/java/com/javastudy/JUNE_2025/Class05/ErrorAndException2.java
@@ -2,16 +2,15 @@ package com.javastudy.JUNE_2025.Class05;
 
 import com.javastudy.JUNE_2025.Class04.Student;
 
-public class ErrorAndException1 {
+public class ErrorAndException2 {
      public static void main(String[] args) {
       /*
        * Exception 1:  check where is the bug
        * Exception 2:  as a retrun, check the condition
        * 
        */
-        Student[]  arr = new Student[3]; // [null,null,null]
-        String name = arr[0].getName(); // arr[0] = null  ==> Null Pointer Exception
-        System.out.println(name);
+        Student  stu = new Student("NAME,123");
+        System.out.println(stu);
         
         //ctrl + click for exception line;
      }
