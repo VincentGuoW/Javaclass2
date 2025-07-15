7752c7e09450a70f36a653930b045a36f5d292d4
VincentGuoW <sdevincentg@gmail.com>
2025-07-15 16:30:59 -0400

    test

---
+++ study/src/main/java/com/javastudy/JULY_2025/IOClass/IOClass03/CharSteamDemo1.java
@@ -0,0 +1,17 @@
+package com.javastudy.JULY_2025.IOClass.IOClass03;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CharSteamDemo1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader  fr =new BufferedReader( new FileReader("study\\src\\main\\java\\com\\javastudy\\JULY_2025\\IOClass\\IOClass03\\test.txt"));
+        
+        int b1 = fr.read();
+        System.out.println(b1);
+
+        int b2 = fr.read();
+        System.out.println(b2);
+    }
+}
