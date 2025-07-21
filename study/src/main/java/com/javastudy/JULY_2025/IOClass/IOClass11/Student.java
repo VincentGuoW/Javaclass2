598ab7f10a1796b410bfaadd6ca7b27e323c4a54
VincentGuoW <sdevincentg@gmail.com>
2025-07-21 08:51:49 -0400

    test

--- study/src/main/java/com/javastudy/JULY_2025/IOClass/IOClass11/Student.java
+++ study/src/main/java/com/javastudy/JULY_2025/IOClass/IOClass11/Student.java
@@ -1,17 +1,10 @@
 package com.javastudy.JULY_2025.IOClass.IOClass11;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 
 public class Student implements Serializable {
-    private static final long serialVersionUID = -4016602135214900592L;
-=======
-import java.io.NotSerializableException;
-import java.io.Serializable;
+    // use implements Serializable!!
 
-public class Student implements Serializable {
-    //use implements Serializable!!
->>>>>>> 21b20e1f3ca6b77e4f933d59eeddb0f1a6b9d322
     private int age;
     private String name;
     private String test;
