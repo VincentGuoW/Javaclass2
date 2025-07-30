6eed1de006e6e2bfd2ab368a215bec0c4d19e71b
VincentGuoW <sdevincentg@gmail.com>
2025-07-30 16:03:43 -0400

    test

--- study/src/main/java/com/javastudy/AUG_2025/webCrawler/myiotest1.java
+++ study/src/main/java/com/javastudy/AUG_2025/webCrawler/myiotest1.java
@@ -1,7 +1,6 @@
 package com.javastudy.AUG_2025.webCrawler;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -9,20 +8,24 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class myiotest1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException, IOException {
         String boyNameWeb = "https://www.cs.cmu.edu/afs/cs/project/ai-repository/ai/areas/nlp/corpora/names/male.txt";
+        String result = webCrawler(boyNameWeb);
+        System.out.println(result);
+
     }
 
-    public static String webCrawler(String inputWeb) throws URISyntaxException, IOException{
+    public static String webCrawler(String inputWeb) throws URISyntaxException, IOException {
         StringBuilder sb = new StringBuilder();
         URI uri = new URI(inputWeb);
         URL url = uri.toURL();
         URLConnection conn = url.openConnection();
         InputStreamReader isr = new InputStreamReader(conn.getInputStream());
         int ch;
-        while ((ch=isr.read())!=-1) { 
-            (char)ch;
+        while ((ch = isr.read()) != -1) {
+            sb.append((char) ch);
         }
+        isr.close();
         return sb.toString();
     }
 }
