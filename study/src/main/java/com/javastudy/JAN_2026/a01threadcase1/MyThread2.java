6e835ff29d8168df1cbe9aba88bf610501f67e49
VincentGuoW <sdevincentg@gmail.com>
2026-01-20 21:53:08 -0500

    test

---
+++ study/src/main/java/com/javastudy/JAN_2026/a01threadcase1/MyThread2.java
@@ -0,0 +1,11 @@
+package com.javastudy.JAN_2026.a01threadcase1;
+
+public class MyThread2 implements Runnable{
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+   
+}
---
+++ study/src/main/java/com/javastudy/JAN_2026/a01threadcase1/ThreadDemo.java
@@ -0,0 +1,20 @@
+package com.javastudy.JAN_2026.a01threadcase1;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        MyThread t3 = new MyThread();
+        t1.setName("Thread_One");
+        t2.setName("Thread_Two");
+        t3.setName("Thread_Three");
+        t1.start();
+        t2.start();
+        t3.run();
+        //must be .start() not .run()
+        //.start ==> will give multiple thread
+        //.run   ==> just keep in same thread
+    }
+    
+    
+}
