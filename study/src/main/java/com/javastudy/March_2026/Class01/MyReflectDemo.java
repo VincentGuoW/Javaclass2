dda7ee76a8ad1966eec671c4ed1544f4d02274ca
VincentGuoW <sdevincentg@gmail.com>
2026-02-23 23:11:34 -0500

    test

--- study/src/main/java/com/javastudy/March_2026/Class01/MyReflectDemo.java
+++ study/src/main/java/com/javastudy/March_2026/Class01/MyReflectDemo.java
@@ -4,12 +4,13 @@ import java.lang.reflect.Constructor;
 
 public class MyReflectDemo {
     public static void main(String[] args) throws ClassNotFoundException {
-
+        System.out.println();
         Class clazz = Class.forName("com.javastudy.March_2026.Class01.Student");
 
         Constructor[] cons = clazz.getConstructors();
         for (Constructor constructor : cons) {
             System.out.println(constructor);
+            
         }
     }
 }
